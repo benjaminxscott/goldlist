296ac1bb6c2d1bb25b44f987cad94c19757329c3
Ben Schmoker <g@d-str.cc>
2015-11-25 21:57:46 +0000

    add user auth w/ google

--- goldlist.py
+++ goldlist.py
@@ -2,7 +2,25 @@
 
 from flask import Flask, render_template, url_for, request, flash, Markup, jsonify
 
+from flask import session
+import random, string, json
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
+# ----- APP CONFIG -----
+
 app = Flask(__name__)
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+
+CLIENT_SECRET = json.loads(
+    open ("client_secrets.json", "r").read())['web']['client_secret']
+
+app.secret_key = CLIENT_SECRET
+
+app.jinja_env.globals['client_id'] = CLIENT_ID 
 
 # ----- DB SETUP -----
 from sqlalchemy import create_engine
@@ -12,9 +30,8 @@ from db_setup import Base, Listing, Location
 engine = create_engine ('sqlite:///listings.db')
 Base.metadata.bind = engine
 
-DBsession = sessionmaker(bind = engine)
-session = DBsession()
-
+buildsession = sessionmaker(bind = engine)
+dbsession = buildsession()
 
 # ----- URL ROUTING -----
 
@@ -24,7 +41,7 @@ session = DBsession()
 def get_inventory():
     # API ENDPOINT
     if request.args.get("format") == "json":
-        inventory = session.query(Listing).all()
+        inventory = dbsession.query(Listing).all()
         json_data = jsonify(GoldListing = [item.serialize for item in inventory])
         return json_data
     else:
@@ -32,7 +49,61 @@ def get_inventory():
     
 @app.route('/login')
 def show_login():
+    # store CSRF token in session
+    session['state'] = get_csrf_token()
     return render_template("login.html")
+
+
+@app.route('/gconnect')
+def do_login():
+    # ref: https://gist.github.com/bschmoker/74187bad5bbd0a0bf336
+
+    # Validate CSRF token
+    if request.args.get('csrf_token') != session['state']:
+        return "Bad CSRF token" # TODO return 401 with JSON headers
+
+    one_time_code = request.data # sent from jquery
+    
+    # exchange one-time-code for server-side access token
+    try:
+        
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(one_time_code)
+    except FlowExchangeError:
+        return "Oauth2 one-time-code was bogus" # TODO return 401 with JSON headers
+    
+    
+# TODO debug output
+    if credentials.access_token:
+        return credentials.access_token 
+    else :
+        return "some error"
+    
+# TODO check whether access_token is valid using curl https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=<my_token>
+
+'''
+TODO grab user info
+    gplus_id = credentials.id_token['sub']
+
+    # store credentials as login session
+    session['credentials'] = credentials
+    session['gplus_id'] = gplus_id
+    
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    session['username'] = data['name']
+    session['picture'] = data['picture']
+    session['email'] = data['email']
+    
+    flash( "Hi there %s" %session['username'] )
+    '''
+
 
 
 @app.route('/listing/new', methods=['GET','POST'])
@@ -57,7 +128,7 @@ def add_inventory():
             
             # validate location exists
             try:
-                location = session.query(Location).filter_by(loc_id = request.form['location_id']).one()
+                location = dbsession.query(Location).filter_by(loc_id = request.form['location_id']).one()
             except:
                 flash("That doesn't look like a valid location") 
                 error = True
@@ -71,8 +142,8 @@ def add_inventory():
         if not error:
             # store in DB        
             new_listing = Listing(name= name, description = description, price = price, location = location )
-            session.add(new_listing)
-            session.commit()
+            dbsession.add(new_listing)
+            dbsession.commit()
             
             # build a flash message safe for rendering (no xss)    
             msg = "Nice! Would you like to  <a href = "  
@@ -90,7 +161,7 @@ def edit_inventory(listing_id):
     changed_listing = None
     
     try:
-        changed_listing = session.query(Listing).filter_by(listing_id = listing_id).one()
+        changed_listing = dbsession.query(Listing).filter_by(listing_id = listing_id).one()
     
     except :
         flash("Looks like that listing doesn't exist")
@@ -106,8 +177,8 @@ def edit_inventory(listing_id):
         if request.form['price']:
             changed_listing.price = request.form['price']
             
-        session.add(changed_listing)
-        session.commit()
+        dbsession.add(changed_listing)
+        dbsession.commit()
         
         flash ("Your changes to %s have been saved " % changed_listing.name)
         
@@ -121,9 +192,9 @@ def delete_inventory(listing_id):
 
     if request.method == 'POST':
         try:
-            toremove_listing = session.query(Listing).filter_by(listing_id = listing_id).one()
-            session.delete(toremove_listing)
-            session.commit()
+            toremove_listing = dbsession.query(Listing).filter_by(listing_id = listing_id).one()
+            dbsession.delete(toremove_listing)
+            dbsession.commit()
             flash ("Your listing for %s has been removed" % toremove_listing.name)
             
         except:
@@ -136,19 +207,23 @@ def delete_inventory(listing_id):
 def show_location(loc_id):
 
     # filter by this location
-    inventory = session.query(Listing).filter_by(loc_id = loc_id) 
+    inventory = dbsession.query(Listing).filter_by(loc_id = loc_id) 
     
     if len( inventory.all() ) == 0:
         flash ("Looks like there is nothing listed at that location")
     
     return render_template("listings.html", inventory=inventory)
 
-# ----- FUNCTIONS  -----
 
-def do_login():
-    return True #login token
 
-# TODO handle getting new token before timeout using refresh token from goog
+# ----- UTILITY FUNCTIONS -----
+
+def get_csrf_token():
+    token = ''
+    for char in xrange(32):
+        token += random.choice(string.ascii_uppercase + string.digits) 
+
+    return token
 
 
 @app.context_processor
@@ -156,13 +231,13 @@ def utility_processor():
 
     def all_locations():
         # list all locations we know about
-        locations = session.query(Location).all()
+        locations = dbsession.query(Location).all()
         
         return locations
         
     def all_listings():
         # list all items in current inventory
-        inventory = session.query(Listing).all()
+        inventory = dbsession.query(Listing).all()
         return inventory
         
     return dict(all_locations=all_locations, all_listings=all_listings)    
@@ -171,6 +246,4 @@ def utility_processor():
 # ----- RUN CONFIG ------
 if __name__ == '__main__':
     app.debug = True
-    with open ("app.cfg", "r") as config:
-        app.secret_key = config.read()
     app.run(host = '0.0.0.0', port = 8080)
